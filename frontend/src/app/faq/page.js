3155b987e1aa3486eeefec348dba526e1696aa24
James Liu <liuxjames88@gmail.com>
2023-11-04 16:17:22 -0400

    fix styling for faq accordion

--- frontend/src/app/faq/page.js
+++ frontend/src/app/faq/page.js
@@ -4,22 +4,24 @@ function Page() {
   return (
   <>
     <h1>FAQ</h1>
-    <section className="accordion">
-  <div className={styles["tab"]}>
-    <input type="checkbox" name="accordion-1" id="cb1" />
-    <label htmlFor="cb1" className={styles["tab__label"]}>Question 1</label>
-    <div className={styles["tab__content"]}>
-      <p>some answer here</p>
-    </div>
+    <div className={styles["accordion-container"]}>
+      <section className={styles["accordion"]}>
+        <div className={styles["tab"]}>
+          <input type="checkbox" name="accordion-1" id="cb1" />
+          <label htmlFor="cb1" className={styles["tab__label"]}>Question 1</label>
+          <div className={styles["tab__content"]}>
+            <p>some answer here</p>
+          </div>
+        </div>
+      <div className={styles["tab"]}>
+        <input type="checkbox" name="accordion-1" id="cb2" />
+        <label htmlFor="cb2" className={styles["tab__label"]}>Question 2</label>
+        <div className={styles["tab__content"]}>
+          <p>another answer here</p>
+        </div>
+      </div>
+    </section>
   </div>
-  <div className={styles["tab"]}>
-    <input type="checkbox" name="accordion-1" id="cb2" />
-    <label htmlFor="cb2" className={styles["tab__label"]}>Question 2</label>
-    <div className={styles["tab__content"]}>
-      <p>another answer here</p>
-    </div>
-  </div>
-</section>
 
   </>);
 
