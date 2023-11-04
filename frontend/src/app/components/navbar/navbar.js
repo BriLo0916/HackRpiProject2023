7452127ca51d13ec83f85ae07c91ea2d136ac9ef
James Liu <liuxjames88@gmail.com>
2023-11-04 15:06:40 -0400

    add page route

--- frontend/src/app/components/navbar/navbar.js
+++ frontend/src/app/components/navbar/navbar.js
@@ -1,5 +1,13 @@
-function Navbar(props) {
-    return <h2>This is some text</h2>;
+import Link from "next/link";
+
+function Navbar() {
+  return ( 
+    <>
+    <h2>This is some text</h2>
+    <Link href="/"> Home </Link>
+    <Link href="/resources"> Reresource pagsources</Link>
+    </>
+  );
 }
 
 export default Navbar;
---
+++ frontend/src/app/resources/page.js
@@ -0,0 +1,13 @@
+
+
+function Page() {
+
+  return (<>
+  <h1>This is the resource page </h1>
+  </>);
+
+
+}
+
+
+export default Page;
