34cc9731916214a1ca5411297ad7b9fb3793036a
Caroline Wu <112650648+wuc13@users.noreply.github.com>
2023-11-05 07:55:03 -0500

    resources
    resources of mental health things

---
+++ frontend/src/app/components/card/card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './card.module.css';
+
+function Card(props) {
+    return (
+        <>
+            <div className={styles["card"]}>
+                <img src={props.image} alt={props.title} />
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+                <br />
+                <a href={props.link} className={styles["card-link"]}>{props.linkDescription}</a>
+            </div>
+        </>
+    );
+    // return (
+    //     <>
+    //         <div className={styles["container"]}>
+    //             <div className={styles["card-container1"]}>
+    //                 <a href="https://www.youtube.com/@Psych2go">Psych2go</a>
+    //             </div>
+    //             <br></br>
+    //             <div className={styles["card-container2"]}>
+    //                 <a href="https://www.apa.org/topics/crisis-hotlines">APA Crisis Hotlines</a>
+    //             </div>
+    //         </div>
+    //     </>
+    // );
+}
+
+export default Card;
--- frontend/src/app/resources/page.js
+++ frontend/src/app/resources/page.js
@@ -1,10 +1,44 @@
-
+import styles from  "./page.module.css";
+import Card from '../components/card/card';
 
 function Page() {
-
-  return (<>
-  <h1>This is the resource page </h1>
-  </>);
+  const cardsData= [
+    {
+      title: 'Headspace',
+      description: 'Meditation and tips for well-being',
+      image: 'src\app\resources\headspace-1.jpg',
+      link: 'https://www.headspace.com/', 
+      linkDescription: 'Headspace',
+    },
+    {
+      title: 'Card 2',
+      description: 'List of hotlines around the world',
+      image: '/suidcide-hotline-image.jpg',
+      link: 'https://www.psychologytoday.com/us/basics/suicide/suicide-prevention-hotlines-resources-worldwide', 
+      linkDescription: 'Suicide Hotlines'
+    },
+    {
+      title: 'Card 3',
+      description: 'Educational videos on mental health',
+      image: '/channels4_profile.jpg',
+      link: 'https://www.youtube.com/@Psych2go', 
+      linkDescription: 'Psych2go'
+    },
+  ]
+  return (
+    <>
+      <div className={styles["resources-containers"]}>
+        {cardsData.map((card, index) => (
+          <Card 
+            key = {index}
+            description={card.description}
+            image={card.image}
+            link={card.link} 
+            linkDescription={card.linkDescription}
+          />
+        ))}
+      </div>
+    </>);
 
 
 }
